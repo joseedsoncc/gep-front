a2f62dd9b7cc0a07ddb5fec36de2b33059dd9dda
joseedsoncc <joseedsoncc@hotmail.com>
2026-04-27 16:15:02 -0300

    removendo comentarios da routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,10 @@ import { EscalaSemanalComponent } from './components/escala-semanal/escala-seman
 import { CadastroPlantaoComponent } from './components/cadastro-plantao/cadastro-plantao.component';
 
 export const routes: Routes = [
-  /*  //{ path: '', redirectTo: '/escala', pathMatch: 'full' },
-  { path: 'profissionais/cadastro', component: CadastroProfissionalComponent },
-  { path: 'profissionais/lista', component: ListaProfissionaisComponent },
-  { path: 'escala', component: EscalaSemanalComponent },
-  //{ path: '**', redirectTo: '/escala' }
-*/
-
   { path: '', redirectTo: '/escala', pathMatch: 'full' },
   { path: 'profissionais/cadastro', component: CadastroProfissionalComponent },
   { path: 'profissionais/lista', component: ListaProfissionaisComponent },
   { path: 'escala', component: EscalaSemanalComponent },
-  { path: 'escala/cadastro', component: CadastroPlantaoComponent }, // Adicionar esta linha
+  { path: 'escala/cadastro', component: CadastroPlantaoComponent },
   { path: '**', redirectTo: '/escala' },
 ];
